612bd801a0a9239c5f86ed8d79a5a16151ab8f68
Miguel Martins <mfproencamartins@gmail.com>
2024-05-27 18:23:56 +0100

    Feat-Read from xlsx

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 // Import dependencies
-const XLSX = require('xlsx');
-const jsontoxml = require('jsontoxml');
+const fs = require("fs");
+const XLSX = require("xlsx");
+const jsontoxml = require("jsontoxml");
 
 // Read the file into memory
-// const workbook = XLSX.read(fs.readFileSync('file-example.xlsx'));
-const workbook = XLSX.readFile('file-example.xlsx');
+// const workbook = XLSX.read(fs.readFileSync("file-example.xlsx"));
+const workbook = XLSX.readFile("file-example.xlsx");
+
+// Convert the XLSX to JSON
+let worksheets = {};
+for (const sheetName of workbook.SheetNames) {
+    // Some helper functions in XLSX.utils generate different views of the sheets:
+    //     XLSX.utils.sheet_to_csv generates CSV
+    //     XLSX.utils.sheet_to_txt generates UTF16 Formatted Text
+    //     XLSX.utils.sheet_to_html generates HTML
+    //     XLSX.utils.sheet_to_json generates an array of objects
+    //     XLSX.utils.sheet_to_formulae generates a list of formulae
+    worksheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+}
+
+// Show the data as JSON
+console.log("json:\n", JSON.stringify(worksheets.Sheet1), "\n\n");
